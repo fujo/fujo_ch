7f59010c9097276f62727651b5f1538ee991bd36
jonathan <jonathan@aquaverde.ch>
2015-10-28 13:44:03 +0100

    housekeeping

--- tpl--materials_overview.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Template Name: Materials overview */
-$context = array();
-$context = Timber::get_context(); 
-$context['post'] = Timber::get_post();
-$context['materials'] = Timber::get_posts('post_type=metaltec_material&orderby=menu_order&order=ASC'); // category
-Timber::render('page.twig', $context);
-?>
